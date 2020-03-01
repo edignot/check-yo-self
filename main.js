f45985918c8c4bdcea1b4822a22acbf6f2e41e91
Ed Ignot <edignot@gmail.com>
2020-02-29 22:16:56 -0700

    todo check tasks, delete from dom

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ var makeTaskListBtn = document.querySelector('.make-task-list-btn');
 var clearAllBtn = document.querySelector('.clear-all-btn btn');
 var filterUrgencyBtn = document.querySelector('.filter-urgency-btn');
 var taskCardsContainer = document.querySelector('.task-cards-container');
-var taskDisplayContainer = document.querySelector('.task-display-container');
+var createTaskMessage = document.querySelector('.create-new-task-message');
 var tasksArray = [];
 
 window.addEventListener('load', pageLoadDisplay);
 body.addEventListener('click', buttonClickHandler);
 newTaskContainer.addEventListener('click', removeNewTask);
+taskCardsContainer.addEventListener('click', toDoClickHandler);
 
 function pageLoadDisplay() {
   // displayToDoList();
@@ -25,28 +26,40 @@ function buttonClickHandler(event) {
   if (event.target.closest('.add-task-btn')) {
     createTaskInstance();
   }
+
   if (event.target.closest('.search-btn')) {
     alert('search');
   }
+
   if (event.target.closest('.make-task-list-btn')) {
     createToDoList();
   }
+
   if (event.target.closest('.clear-all-btn')) {
-    clearAllInputs();
+    clearAllData();
   }
+
   if (event.target.closest('.filter-urgency-btn')) {
     alert('filter by urgency');
   }
 }
 
-function displayMessage() {
-  // add condition if there are no other notes to display
-  taskDisplayContainer.insertAdjacentHTML('afterbegin', `
-    <p class="create-new-task-message">Nothing to Display.</br> Create New Task!</p>
-  `);
+function toDoClickHandler() {
+  if (event.target.classList.contains('checkbox-img')) {
+    checkTaskDom(event);
+  }
+
+  if (event.target.classList.contains('delete-img')) {
+    deleteTaskDom(event);
+  }
 }
 
-////////////////////////////////////////////////////////////////////////////////////
+function displayMessage() {
+  if (!localStorage.getItem('toDoArray') || localStorage.getItem('toDoArray') == []) {
+    createTaskMessage.classList.remove('hide');
+  }
+}
+
 function createTaskInstance() {
   if (taskItemInput.value !== '') {
     var task = {
@@ -56,6 +69,8 @@ function createTaskInstance() {
     };
     saveTaskInstance(task);
     displayNewTask(task);
+  } else {
+    alert('Add New Task');
   }
 }
 
@@ -94,9 +109,18 @@ function clearAllInputs() {
   newTaskContainer.innerText = '';
   tasksArray = [];
 }
-////////////////////////////////////////////////////////////////////////////////////
+
+function clearAllData() {
+  taskCardsContainer.innerText = '';
+
+  // toDo.deleteFromStorage();
+  localStorage.removeItem('toDoArray');
+  displayMessage();
+}
+
 function createToDoList() {
   if (taskTitleInput.value !== '' && tasksArray.length > 0) {
+    createTasksArray();
     saveToDoList();
     displayToDoList();
     clearAllInputs();
@@ -107,8 +131,13 @@ function createToDoList() {
   }
 }
 
+function createTasksArray() {
+  if (!localStorage.getItem('toDoArray')) {
+    localStorage.setItem('toDoArray', JSON.stringify([]));
+  }
+}
+
 function saveToDoList() {
-  debugger;
   var id = Date.now();
   var title = taskTitleInput.value;
   var tasks = tasksArray;
@@ -117,12 +146,12 @@ function saveToDoList() {
 }
 
 function displayToDoList() {
+  createTaskMessage.classList.add('hide');
   var titleItem = taskTitleInput.value;
   taskCardsContainer.insertAdjacentHTML('beforeend', `
 <div class="task-card-container">
   <p class="task-card-title">${titleItem}</p>
   <div class="task-list-wrapper" id="${titleItem}">
-
   </div>
   <div class="task-card-footer">
     <div class="urgent-img-wrapper">
@@ -141,9 +170,49 @@ function displayToDoList() {
     taskItem.innerHTML += `
     <div class="task-list-item" id="${tasksArray[i].id}">
       <img src="img/checkbox.svg" alt="checkbox" class="checkbox-img img" id="${tasksArray[i].id}">
+      <img src="img/checkbox-active.svg" alt="checkbox" class="checkbox-img img hide" id="${tasksArray[i].id}">
       <p>${tasksArray[i].text}</p>
     </div>
     `;
   }
+
   clearAllInputs();
 }
+
+function checkTaskDom(event) {
+  event.target.closest('div').classList.add('checked-text');
+  event.target.classList.add('hide');
+  event.target.nextSibling.classList.remove('hide');
+}
+
+function deleteTaskDom(event) {
+  event.target.closest('.task-card-container').remove();
+}
+
+
+
+
+// <div class="task-card-container task-card-container-urgent">
+//   <p class="task-card-title task-card-title-urgent">Task Title</p>
+//   <div class="task-list-wrapper task-list-wrapper-urgent">
+//     <div class="task-list-item">
+//       <img src="img/checkbox.svg" alt="checkbox" class="checkbox-img img">
+//       <p>Your task here!</p>
+//     </div>
+//     <div class="task-list-item">
+//       <img src="img/checkbox.svg" alt="checkbox" class="checkbox-img img">
+//       <p>Your task here!</p>
+//     </div>
+//   </div>
+//   <div class="task-card-footer task-list-footer-urgent">
+//     <div class="urgent-img-wrapper">
+//       <img src="img/urgent.svg" alt="urgent" class="urgent-img img hide">
+//       <img src="img/urgent-active.svg" alt="urgent active" class="urgent-active-img img">
+//       <p class="urgent-active-text">URGENT</p>
+//     </div>
+//     <div class="delete-img-wrapper">
+//       <img src="img/delete.svg" alt="delete" class="delete-img img">
+//       <p class="delete-text">DELETE</p>
+//     </div>
+//   </div>
+// </div>
